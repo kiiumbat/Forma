24f4248015d4bfe375cd1778ea102f80786a8cee
unknown <sergiogalafdz@gmail.com>
2026-02-28 13:12:17 +0100

    Set up 2.0

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import { Forma3DLogo } from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
