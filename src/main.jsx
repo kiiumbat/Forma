14fa1dd63cd7f79f55d2d1c091bdb3c0f7709e9a
unknown <sergiogalafdz@gmail.com>
2026-02-28 13:14:29 +0100

    Set up 3.0

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Forma3DLogo } from './App.jsx'
+import { Forma3D } from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Forma3DLogo />
+    <Forma3D />
   </StrictMode>,
 )
