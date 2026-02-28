a4f99178212eaa3504298c9f949f754ed412b742
unknown <sergiogalafdz@gmail.com>
2026-02-28 13:18:14 +0100

    Set up 4.0

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Forma3D } from './App.jsx'
+import Forma3D from './App'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
